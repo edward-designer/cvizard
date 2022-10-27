db658cb479d5099faebc2094a6f5105fcf4988bb
gracedward <edward.chung.designer@gmail.com>
2022-10-27 13:23:05 +0100

    test: fix failing tests for SVG imports with temp workaround

--- src/__tests__/pages/404.test.tsx
+++ src/__tests__/pages/404.test.tsx
@@ -2,6 +2,13 @@ import { render, screen } from '@testing-library/react';
 
 import NotFoundPage from '@/pages/404';
 
+// workaround for not being able to render SVG imports
+// tried https://github.com/vercel/next.js/issues/35634  https://lightrun.com/answers/storybookjs-storybook-using-svg-as-react-component-inside-story-causes-error---v600-rc26
+jest.mock('~/svg/*.svg', () => ({
+  __esModule: true,
+  default: () => <div />,
+}));
+
 describe('404', () => {
   it('renders a heading', () => {
     render(<NotFoundPage />);
